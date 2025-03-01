cc63ebedbaafdf8798e6411fd131ec06ad649baf
𝖒𝖚𝖘𝖙𝖆𝖋𝖆 <121257754+mustafaakalin@users.noreply.github.com>
2025-03-01 09:32:25 +0300

    Initialize C# project with .NET 9.0 framework and basic area calculation functionality

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+internal class Program
+{
+    public static float AlanHesapla(float k_kenar, float u_kenar)
+    {
+        return k_kenar * u_kenar;
+    }
+
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("alan hesabı için kısa kenar ölçüsü giriniz: ");
+        float k_kenar = Convert.ToSingle(Console.ReadLine());
+        Console.WriteLine("alan hesabı için uzun kenar ölçüsü giriniz: ");
+        float u_kenar = Convert.ToSingle(Console.ReadLine());
+        // float k_kenar = 5;
+        // float u_kenar = 10;
+        float alan = AlanHesapla(k_kenar, u_kenar);
+        System.Console.WriteLine("Alan: " + alan);
+    }
+}
